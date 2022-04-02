cce05cc4d2eddfd78da735573327281769c54d9f
Sven Scharmentke <sven.scharmentke@liasoft.de>
2022-04-02 18:20:22 +0200

    Documentation: Startup

--- Examples/Ajuna.RestService/Startup.cs
+++ Examples/Ajuna.RestService/Startup.cs
@@ -16,20 +16,35 @@ using System.Threading;
 
 namespace Ajuna.RestService
 {
+    /// <summary>
+    /// This class implements configuration and setting up services.
+    /// </summary>
     public class Startup
     {
         private readonly CancellationTokenSource CTS = new CancellationTokenSource();
 
         private readonly StorageSubscriptionChangeDelegate _storageChangeDelegate = new StorageSubscriptionChangeDelegate();
 
+        /// <summary>
+        /// >> Startup
+        /// Constructs and initializes the Startup class.
+        /// Stores the configuration object
+        /// </summary>
+        /// <param name="configuration">The service configuration.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Retreives the service configuration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container. 
+        /// </summary>
+        /// <param name="services">Service collection to configure.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             // Configure web sockets to allow clients to subscribe to storage changes.
@@ -71,7 +86,12 @@ namespace Ajuna.RestService
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
+        /// </summary>
+        /// <param name="app">Application builder</param>
+        /// <param name="env">Service hosting environment</param>
+        /// <param name="handler">Middleware to handle web socket subscriptions</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, StorageSubscriptionHandler handler)
         {
             if (env.IsDevelopment())
