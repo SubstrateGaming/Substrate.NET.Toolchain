4e810a4c1a5dd108a17c8afe7cb7a488c2506cb3
Sven Scharmentke <sven.scharmentke@liasoft.de>
2022-04-02 18:27:28 +0200

    Documentation: StorageChangeMessage, StorageSubscribeMessage, StorageSubscribeMessageResult

--- Examples/Ajuna.RestService/Subscription/Model/StorageSubscribeMessageResult.cs
+++ Examples/Ajuna.RestService/Subscription/Model/StorageSubscribeMessageResult.cs
@@ -2,8 +2,18 @@
 
 namespace Ajuna.RestService.Subscription.Model
 {
+    /// <summary>
+    /// This class implements a serializable status message that indicates the 
+    /// actual status of the subscribe operation.
+    /// </summary>
     public class StorageSubscribeMessageResult
     {
+        /// <summary>
+        /// The actual status code that indicates if the subscription was successful or not.
+        /// At this point this uses regular HttpStatus codes to indicate its status.
+        /// - 200: Success
+        /// - 400: BadRequest
+        /// </summary>
         [JsonProperty("status")]
         public int Status { get; set; }
     }
