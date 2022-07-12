170bac3f631c39fa0a8892f7e8b79e643bf8e46d
Dimitris Paxinos <dpaxinos@gmail.com>
2022-07-12 14:57:04 +0300

    Update StoragePersistenceChangeDelegate.cs

--- Ajuna.AspNetCore/Persistence/StoragePersistenceChangeDelegate.cs
+++ Ajuna.AspNetCore/Persistence/StoragePersistenceChangeDelegate.cs
@@ -9,55 +9,49 @@ namespace Ajuna.AspNetCore.Persistence
    /// </summary>
    public class StoragePersistenceChangeDelegate : IStorageChangeDelegate
    {
-      private readonly string _csvFileName;
+      private readonly string _csvFilePath;
 
-      /// <summary>
-      /// 
-      /// </summary>
-      /// <param name="csvFilePath"> File Path of the CSV file</param>
-      public StoragePersistenceChangeDelegate(string csvFileName = null)
+      /// <param name="fileDirectory"> File Path where the CSV file will be located</param>
+      public StoragePersistenceChangeDelegate(string fileDirectory = null)
       {
-         _csvFileName = csvFileName ?? $"TempChangesStorage_{DateTimeOffset.Now}";
+         _csvFilePath = Path.Combine(fileDirectory ?? String.Empty, $"Storage_Changes_{DateTime.Now:yyyyMMdd_HH.mm.ss}.csv");
       }
       
       public void OnUpdate(string identifier, string key, string data)
       {
          var change = new ChangeRecord(key, identifier, ChangeAction.Update, data);
          StoreChange(change);
-         System.Console.WriteLine("StoragePersistenceChangeDelegate: OnUpdate triggered");
       }
 
       public void OnDelete(string identifier, string key, string data)
       {
          var change = new ChangeRecord(key, identifier, ChangeAction.Delete, data);
          StoreChange(change);
-         System.Console.WriteLine("StoragePersistenceChangeDelegate: OnDelete triggered");
       }
 
       public void OnCreate(string identifier, string key, string data)
       {
          var change = new ChangeRecord(key, identifier, ChangeAction.Create, data);
          StoreChange(change);
-         System.Console.WriteLine("StoragePersistenceChangeDelegate: OnCreate triggered");
       }
 
       private void StoreChange(ChangeRecord changeRecord)
       {
-         if (!File.Exists(_csvFileName))
+         if (!File.Exists(_csvFilePath))
          {
             // Create a file to write to.
-            using (StreamWriter sw = File.CreateText(_csvFileName))
+            using (StreamWriter sw = File.CreateText(_csvFilePath))
             {
                sw.WriteLine("Key,Identifier,Action,Data,UpdateDate");
-            }	
+            }
          }
 
          // This text is always added, making the file longer over time
          // if it is not deleted.
-         using (StreamWriter sw = File.AppendText(_csvFileName))
+         using (StreamWriter sw = File.AppendText(_csvFilePath))
          {
             sw.WriteLine(String.Join(",",changeRecord.Key, changeRecord.Identifier, changeRecord.Action, changeRecord.Data, changeRecord.UpdateDate));
-         }	
+         }
       }
    }
 }
