2ea72da177ec5d90f21225cd0da22003b1ac8296
Cedric Decoster <darkfriend77@users.noreply.github.com>
2024-09-12 08:23:09 +0200

    updated to 0.6.7 added new rust enum version (#83)
    * updated to 0.6.7 added new rust enum version
    updated net api to 24-rc5
    
    * updated comments

--- Tools/Substrate.DotNet/Service/Node/Base/BuilderBase.cs
+++ Tools/Substrate.DotNet/Service/Node/Base/BuilderBase.cs
@@ -7,14 +7,13 @@ using System.Linq;
 
 namespace Substrate.DotNet.Service.Node.Base
 {
-
    public abstract class BuilderBase
    {
       public static readonly List<string> Files = new();
 
       public uint Id { get; }
-      
-      NodeTypeResolver Resolver { get; }
+
+      private NodeTypeResolver Resolver { get; }
 
       public bool Success { get; set; }
 
@@ -153,7 +152,7 @@ namespace Substrate.DotNet.Service.Node.Base
 
          space.Add((FileName is null ? ClassName : FileName) + ".cs");
 
-         // Remove the first two parts of the namespace to avoid the files being created in the Substrate/NetApi sub folder. 
+         // Remove the first two parts of the namespace to avoid the files being created in the Substrate/NetApi sub folder.
          space = space.TakeLast(space.Count - 2).ToList();
 
          // Add base path at the beginning of the paths list
@@ -176,8 +175,25 @@ namespace Substrate.DotNet.Service.Node.Base
          targetClass.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference("SubstrateNodeType"), new CodeAttributeArgument(
             new CodeSnippetExpression($"TypeDefEnum.{typeDef.TypeDef}")
          )));
-
       }
 
+      public static string HandleReservedKeyword(string name)
+      {
+         // List of C# reserved keywords
+         var reservedKeywords = new HashSet<string>
+          {
+              "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class",
+              "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event",
+              "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if",
+              "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null",
+              "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly",
+              "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct",
+              "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort",
+              "using", "virtual", "void", "volatile", "while"
+          };
+
+         // If the name is a reserved keyword, prepend with @
+         return reservedKeywords.Contains(name) ? $"@{name}" : name;
+      }
    }
 }
